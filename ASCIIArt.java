419cb6f265933c300c58fe53793beb8c644131ff
Xunyuan Sun <32882822+Holy233@users.noreply.github.com>
2018-08-27 17:58:28 -0400

    ...

---
+++ ASCIIArt.java
@@ -0,0 +1,27 @@
+/**
+ * ASCIIArt
+ * @author Xunyuan Sun CMPSC 221
+ */
+
+public class Main 
+{
+  
+   public static void main(String[] args) 
+   {
+      System.out.print(" *****  ");
+      System.out.println(" ***** ");
+      System.out.print(" * ");
+      System.out.println("      * ");
+      System.out.print(" * ");
+      System.out.println("      * ");
+      System.out.print(" ****** ");
+      System.out.println(" ****** ");
+      System.out.print(" *    * ");
+      System.out.println(" *    * ");
+      System.out.print(" *    * ");
+      System.out.println(" *    *");
+      System.out.print(" ******");
+      System.out.println("  ******");
+  }
+  
+}
